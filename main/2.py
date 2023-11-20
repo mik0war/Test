bf8214c6f3201886199df9c3d4df72ecc32d5e9b
korneevDev <korneev.dev@gmail.com>
2023-11-20 11:57:28 +0300

    Second commit

---
+++ main/2.py
@@ -0,0 +1,7 @@
+import main.main
+
+a = 0
+print(a)
+
+if __name__ == '__main__':
+    main.main.my_function(9)
---
+++ main/main.py
@@ -0,0 +1,17 @@
+class MyClass:
+
+    def __init__(self, name) -> None:
+        super().__init__()
+        self.name = name
+
+    def test(self):
+        print(self.name)
+
+    @staticmethod
+    def my_pow(a):
+        return a * a
+
+
+
+
+print(MyClass.my_pow(5))
